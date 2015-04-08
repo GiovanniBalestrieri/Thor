3185e6a3a73fe07b83086ef03511d7ae53882469
Marco Passeri <mbass.pass@gmail.com>
2015-04-08 14:18:31 +0200

    Aggiornato arduino_nav_bridge con i frame_id

--- catkin_ws/src/arduino_nav_bridge/src/arduino_com_node_imu.cpp
+++ catkin_ws/src/arduino_nav_bridge/src/arduino_com_node_imu.cpp
@@ -14,8 +14,10 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "imu_bridge");	
 	ros::NodeHandle n;
 		
-	/**** COVARIANZE ****/
+	/**** COVARIANZE E COSTANTI ****/
 	set_covariance();
+	imu_msg.header.frame_id = "imu";
+	odom.header.frame_id = "wheelodom";
 	
 	/**** GESTIONE TOPIC ****/	
 	ros::Subscriber quatsub = n.subscribe("quaternion", 1000, quat_callback);
@@ -26,8 +28,7 @@ int main(int argc, char **argv){
 	ros::Subscriber twistsub = n.subscribe("odom_twist", 1000, twist_callback);
 	
 	imu_pub = n.advertise<sensor_msgs::Imu>("imu_data", 1000);
-	odom_pub = n.advertise<nav_msgs::Odometry>("pr2_base_odometry/odom", 1000);
-	//odom_pub = n.advertise<nav_msgs::Odometry>("odom", 1000);
+	odom_pub = n.advertise<nav_msgs::Odometry>("odom", 1000);
 	
 	ros::spin();
 	
@@ -36,110 +37,111 @@ int main(int argc, char **argv){
 
 void set_covariance(){
 	// Covarianze IMU
-	imu_msg.orientation_covariance[0] = 0.0;
+	imu_msg.orientation_covariance[0] = 1.0;
 	imu_msg.orientation_covariance[1] = 0.0;
 	imu_msg.orientation_covariance[2] = 0.0;
 	imu_msg.orientation_covariance[3] = 0.0;
-	imu_msg.orientation_covariance[4] = 0.0;
+	imu_msg.orientation_covariance[4] = 1.0;
 	imu_msg.orientation_covariance[5] = 0.0;
 	imu_msg.orientation_covariance[6] = 0.0;
 	imu_msg.orientation_covariance[7] = 0.0;	
-	imu_msg.orientation_covariance[8] = 0.0;
+	imu_msg.orientation_covariance[8] = 1.0;
 	
-	imu_msg.angular_velocity_covariance[0] = 0.0;
+	imu_msg.angular_velocity_covariance[0] = 1.0;
 	imu_msg.angular_velocity_covariance[1] = 0.0;
 	imu_msg.angular_velocity_covariance[2] = 0.0;
 	imu_msg.angular_velocity_covariance[3] = 0.0;
-	imu_msg.angular_velocity_covariance[4] = 0.0;
+	imu_msg.angular_velocity_covariance[4] = 1.0;
 	imu_msg.angular_velocity_covariance[5] = 0.0;
 	imu_msg.angular_velocity_covariance[6] = 0.0;
 	imu_msg.angular_velocity_covariance[7] = 0.0;
-	imu_msg.angular_velocity_covariance[8] = 0.0;
+	imu_msg.angular_velocity_covariance[8] = 1.0;
 
-	imu_msg.linear_acceleration_covariance[0] = 0.0;
+	imu_msg.linear_acceleration_covariance[0] = 1.0;
 	imu_msg.linear_acceleration_covariance[1] = 0.0;
 	imu_msg.linear_acceleration_covariance[2] = 0.0;
 	imu_msg.linear_acceleration_covariance[3] = 0.0;
-	imu_msg.linear_acceleration_covariance[4] = 0.0;
+	imu_msg.linear_acceleration_covariance[4] = 1.0;
 	imu_msg.linear_acceleration_covariance[5] = 0.0;
 	imu_msg.linear_acceleration_covariance[6] = 0.0;
 	imu_msg.linear_acceleration_covariance[7] = 0.0;
-	imu_msg.linear_acceleration_covariance[8] = 0.0;
+	imu_msg.linear_acceleration_covariance[8] = 1.0;
 	
 	//Covarianze odometry
-	odom.pose.covariance[0] = 0.0;
+	odom.pose.covariance[0] = 1.0;
 	odom.pose.covariance[1] = 0.0;
 	odom.pose.covariance[2] = 0.0;
 	odom.pose.covariance[3] = 0.0;
 	odom.pose.covariance[4] = 0.0;
 	odom.pose.covariance[5] = 0.0;
 	odom.pose.covariance[6] = 0.0;
-	odom.pose.covariance[7] = 0.0;
+	odom.pose.covariance[7] = 1.0;
 	odom.pose.covariance[8] = 0.0;
 	odom.pose.covariance[9] = 0.0;
 	odom.pose.covariance[10] = 0.0;
 	odom.pose.covariance[11] = 0.0;
 	odom.pose.covariance[12] = 0.0;
 	odom.pose.covariance[13] = 0.0;
-	odom.pose.covariance[14] = 0.0;
+	odom.pose.covariance[14] = 1.0;
 	odom.pose.covariance[15] = 0.0;
 	odom.pose.covariance[16] = 0.0;
 	odom.pose.covariance[17] = 0.0;
 	odom.pose.covariance[18] = 0.0;
 	odom.pose.covariance[19] = 0.0;
 	odom.pose.covariance[20] = 0.0;
-	odom.pose.covariance[21] = 0.0;
+	odom.pose.covariance[21] = 1.0;
 	odom.pose.covariance[22] = 0.0;
 	odom.pose.covariance[13] = 0.0;
 	odom.pose.covariance[24] = 0.0;
 	odom.pose.covariance[25] = 0.0;
 	odom.pose.covariance[26] = 0.0;
 	odom.pose.covariance[27] = 0.0;
-	odom.pose.covariance[28] = 0.0;
+	odom.pose.covariance[28] = 1.0;
 	odom.pose.covariance[29] = 0.0;
 	odom.pose.covariance[30] = 0.0;
 	odom.pose.covariance[31] = 0.0;
 	odom.pose.covariance[32] = 0.0;
 	odom.pose.covariance[33] = 0.0;
 	odom.pose.covariance[34] = 0.0;
-	odom.pose.covariance[35] = 0.0;
+	odom.pose.covariance[35] = 1.0;
 	
-	odom.twist.covariance[0] = 0.0;
+	odom.twist.covariance[0] = 1.0;
 	odom.twist.covariance[1] = 0.0;
 	odom.twist.covariance[2] = 0.0;
 	odom.twist.covariance[3] = 0.0;
 	odom.twist.covariance[4] = 0.0;
 	odom.twist.covariance[5] = 0.0;
 	odom.twist.covariance[6] = 0.0;
-	odom.twist.covariance[7] = 0.0;
+	odom.twist.covariance[7] = 1.0;
 	odom.twist.covariance[8] = 0.0;
 	odom.twist.covariance[9] = 0.0;
 	odom.twist.covariance[10] = 0.0;
 	odom.twist.covariance[11] = 0.0;
 	odom.twist.covariance[12] = 0.0;
 	odom.twist.covariance[13] = 0.0;
-	odom.twist.covariance[14] = 0.0;
+	odom.twist.covariance[14] = 1.0;
 	odom.twist.covariance[15] = 0.0;
 	odom.twist.covariance[16] = 0.0;
 	odom.twist.covariance[17] = 0.0;
 	odom.twist.covariance[18] = 0.0;
 	odom.twist.covariance[19] = 0.0;
 	odom.twist.covariance[20] = 0.0;
-	odom.twist.covariance[21] = 0.0;
+	odom.twist.covariance[21] = 1.0;
 	odom.twist.covariance[22] = 0.0;
 	odom.twist.covariance[13] = 0.0;
 	odom.twist.covariance[24] = 0.0;
 	odom.twist.covariance[25] = 0.0;
 	odom.twist.covariance[26] = 0.0;
 	odom.twist.covariance[27] = 0.0;
-	odom.twist.covariance[28] = 0.0;
+	odom.twist.covariance[28] = 1.0;
 	odom.twist.covariance[29] = 0.0;
 	odom.twist.covariance[30] = 0.0;
 	odom.twist.covariance[31] = 0.0;
 	odom.twist.covariance[32] = 0.0;
 	odom.twist.covariance[33] = 0.0;
 	odom.twist.covariance[34] = 0.0;
-	odom.twist.covariance[35] = 0.0;
+	odom.twist.covariance[35] = 1.0;
+	
 }
 
 void sendImu(int rec){
@@ -148,7 +150,6 @@ void sendImu(int rec){
 
 		/**** INVIO MESSAGGIO IMU ****/
 		imu_msg.header.stamp = ros::Time::now();
-		imu_msg.header.frame_id = "imu_link";
 		imu_pub.publish(imu_msg);
 
 		/**** RESET STATO DI RICEZIONE DATI ****/
@@ -212,7 +213,7 @@ void twist_callback(const geometry_msgs::Twist t){
 void sendOdometry(int type){
 	send_status_odom[type] = 1;
 	if(send_status_odom[0] == 1 && send_status_odom[1] == 1){
-		odom.header = imu_msg.header;
+		odom.header.stamp = ros::Time::now();
 		odom_pub.publish(odom);
 		send_status_odom[0] = 0;
 		send_status_odom[1] = 0;
